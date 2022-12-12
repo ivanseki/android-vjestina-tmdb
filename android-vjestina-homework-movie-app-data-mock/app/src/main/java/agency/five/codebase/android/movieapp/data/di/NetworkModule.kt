ecb4b5540c42ce56ae617cf729311e021d597178
“ivan.sekeres” <ivan.chernobyl007@gmail.com>
2022-12-12 11:35:33 +0100

    Implement MovieService

--- android-vjestina-homework-movie-app-data-mock/app/src/main/java/agency/five/codebase/android/movieapp/data/di/NetworkModule.kt
+++ android-vjestina-homework-movie-app-data-mock/app/src/main/java/agency/five/codebase/android/movieapp/data/di/NetworkModule.kt
@@ -1,9 +1,12 @@
 package agency.five.codebase.android.movieapp.data.di
 
 import agency.five.codebase.android.movieapp.data.network.MovieService
+import agency.five.codebase.android.movieapp.data.network.MovieServiceImpl
 import io.ktor.client.*
 import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.*
 import org.koin.dsl.module
 
 val networkModule = module {
@@ -12,9 +15,14 @@ val networkModule = module {
     single {
         HttpClient(Android) {
             expectSuccess = true
+
             install(Logging) {
                 logger = Logger.DEFAULT
                 level = LogLevel.HEADERS
+            }
+
+            install(ContentNegotiation) {
+                json()
             }
         }
     }
